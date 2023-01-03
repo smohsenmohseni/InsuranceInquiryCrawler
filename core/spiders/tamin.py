35f77b729bdbb0a8e06ef8c465fcdca0a7c88699
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-03 17:54:02 +0330

    FEAT: add tamin insurance

---
+++ core/spiders/tamin.py
@@ -0,0 +1,12 @@
+import json
+# Core imports.
+from scrapy import Spider
+from scrapy.http import Request
+
+
+class TaminInsuranceSpider(Spider):
+    name = 'tamin_insurance'
+    start_urls = ['https://medical.tamin.ir/api/medical-support/v2.0/2051057540']
+
+    def parse(self, response, **kwargs):
+        return json.loads(response.body)
