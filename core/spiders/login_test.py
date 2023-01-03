b68e666056255c9e32a0784cd548345943e1892a
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-03 15:43:15 +0330

    FEAT: try other spider and check by local server

--- core/spiders/login_test.py
+++ core/spiders/login_test.py
@@ -1,18 +1,54 @@
+# Standard imports
+import json
+
 # Core imports.
 import scrapy
+from scrapy.http import Request, FormRequest, JsonRequest
 
 
 class LoginSpider(scrapy.Spider):
-    name = 'dlogin'
-    start_urls = ['http://localhost:8000/admin/login/']
+    name = 'login_to'
+    start_urls = ['https://mccp.iraneit.com/core/connect/token']
+    headers_ = {
+        'user-agent': (
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        ),
+        'origin': 'https://mccp.iraneit.com',
+        'referer': 'https://mccp.iraneit.com/',
+    }
+    meta_ = {'dont_redirect': True, 'handle_httpstatus_list': [302, 401]}
 
-    def parse(self, response):
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata={'username': '09012223218', 'password': 'mohsn'},
-            callback=self.after_login,
-            meta={'dont_redirect': True, 'handle_httpstatus_list': [302]},
+    def start_requests(self):
+        _data = {
+            'scope': 'openid profile user_info',
+            'grant_type': 'password',
+            'username': 'l.tehran11001',
+            'password': 'abc123',
+            'client_id': 'MCClaimProc-ResOwner',
+            'client_secret': 'secret',
+        }
+        req = FormRequest(
+            self.start_urls[0], formdata=_data, headers=self.headers_, callback=self.parse, meta=self.meta_
         )
+        yield req
 
-    def after_login(self, response):
-        print(response.headers)
+    def parse(self, response, **kwargs):
+        _url = (
+            "https://mccp.iraneit.com/odata/MCClaimProc/preAuthEnabledPolicy/getInsuredPersonPolicyInfo"
+            "(corpId=155,nationalCodeOrId='2051057540',type='nationalcode')?$top=1"
+        )
+        headers_with_cookie = dict()
+        headers_with_cookie.update(self.headers_)
+        headers_with_cookie.update(json.loads(response.body))
+        headers_with_cookie.update(
+            {
+                'MCCP_ARR': '753befbf0e269336bbc36a7115522b190547b2e11c6a3b0aa89bad9cbdad411d',
+                'accept': 'application/json;q = 0.9, */*;q=0.1',
+            }
+        )
+        req = JsonRequest(_url, headers=headers_with_cookie, callback=self.parse2, meta=self.meta_)
+        yield req
+
+    def parse2(self, response, **kwargs):
+        print(response.status)
+        print(response.request.headers)
--- etc/settings.py
+++ etc/settings.py
@@ -3,7 +3,9 @@ BOT_NAME = 'core'
 SPIDER_MODULES = ['core.spiders']
 NEWSPIDER_MODULE = 'core.spiders'
 
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
 TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
+
+LOG_ENABLED = False
