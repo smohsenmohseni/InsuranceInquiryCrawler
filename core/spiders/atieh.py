e4ebe5c30fec490f19ad51f2d3f5cd29cb3dedd8
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-06 09:40:37 +0330

    FEAT: add atieh insurance crawler

--- core/spiders/atieh.py
+++ core/spiders/atieh.py
@@ -35,7 +35,7 @@ class AtiehInsuranceSpider(Spider):
             method='POST',
             cookies={'TGC': token},
             formdata={
-                'nationalCode': '1930577011',
+                'nationalCode': '2590453711',
                 'requestType': 'outpatient',
                 '_eventId': '',
             },
@@ -45,14 +45,16 @@ class AtiehInsuranceSpider(Spider):
         )
 
     def parse(self, response, **kwargs):
-        print('\n'*5)
-        print('response:', response.url)
-        rows = response.css('#insuredList tbody tr')
-        for row in rows:
-            values = row.css('td *::text').getall()
-            yield {
-                'name': values[0],
-                'insurance': values[2],
-                'start_date': values[5],
-                'expire_date': values[6],
-            }
+        rows = response.css(
+            '#policyInfoPanelBox-collapse div.col-lg-4.col-md-4.col-sm-6.col-xs-12 '
+            'p.text-secondary.pd-top-label-body.iransans'
+        )
+        values = rows.css('*::text').getall()
+        yield {
+            'name': values[0],
+            'national_code': values[1],
+            'father_name': values[2],
+            'birthdate': values[4],
+            'insurance_name': values[6],
+            'basic_insurance_name': values[12],
+        }
