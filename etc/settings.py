1b3a9ccd4572edbe6f8a63dcde9869b4f981310a
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-06 18:00:26 +0330

    FEAT: update user agent

--- etc/settings.py
+++ etc/settings.py
@@ -5,7 +5,9 @@ SPIDER_MODULES = ['app.spiders']
 NEWSPIDER_MODULE = 'app.spiders'
 SPIDER_LOADER_CLASS = 'core.spiderloader.SpiderLoader'
 
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+USER_AGENT = (
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+)
 
 REDIRECT_ENABLED = False
 RETRY_ENABLED = False
