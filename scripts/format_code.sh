18427028f5f9ecf3fe205b2b0125edbce66a725a
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-07 23:11:50 +0330

    CLEAN: add shell script to check required configuration

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -1,1 +1,11 @@
-isort .; black .; mypy .;
+#!/bin/bash
+
+THERE_IS_NO_CONFIGURATION_FILE='There is no configuration file.'
+
+
+if [ ! -f ./pyproject.toml ] || [ ! -f ./.isort.cfg ] || [ ! -f ./mypy.ini ]
+then
+  echo -e "\n\033[31m${THERE_IS_NO_CONFIGURATION_FILE}\033[0m\n"
+else
+  isort . && echo -e \\n; black . && echo -e \\n; mypy .;
+fi
