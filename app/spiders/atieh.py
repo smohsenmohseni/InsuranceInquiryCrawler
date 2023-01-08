448ab1be8023717da0d5f31b502d5e998422debc
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-08 14:58:59 +0330

    FEAT: validate atieh insurance

--- app/spiders/atieh.py
+++ app/spiders/atieh.py
@@ -5,8 +5,12 @@ from http.cookies import SimpleCookie
 # Core imports.
 from scrapy.http import Request, FormRequest, TextResponse
 
+# Third-party imports.
+import json5
+
 # Local imports.
 from app.generics import GenericSpider
+from app.helpers.decorator import disable_cache
 
 
 class AtiehInsuranceSpider(GenericSpider):
@@ -22,10 +26,11 @@ class AtiehInsuranceSpider(GenericSpider):
             callback=self.inquiry_request,
         )
 
+    @disable_cache
     def inquiry_request(self, response: TextResponse) -> FormRequest:
         c: SimpleCookie = SimpleCookie()
         c.load(response.request.headers.get('Cookie').decode())
-        return FormRequest(
+        yield FormRequest(
             self.inquiry_url,
             method='POST',
             cookies={'JSESSIONID': c['JSESSIONID'].value},
@@ -37,17 +42,22 @@ class AtiehInsuranceSpider(GenericSpider):
             callback=self.parse,
         )
 
-    def parse(self, response: TextResponse, **kwargs: None) -> dict:
+    def parse(self, response: TextResponse, **kwargs: None) -> Generator[dict, None, None]:
         rows = response.css(
             '#policyInfoPanelBox-collapse div.col-lg-4.col-md-4.col-sm-6.col-xs-12 '
             'p.text-secondary.pd-top-label-body.iransans'
         )
         values: list[str] = rows.css('*::text').getall()
-        return {
-            'name': values[0],
-            'national_code': values[1],
-            'father_name': values[2],
-            'birthdate': values[4],
-            'insurance_name': values[6],
-            'basic_insurance_name': values[12],
-        }
+        if values:
+            yield {
+                'name': values[0],
+                'national_code': values[1],
+                'father_name': values[2],
+                'birthdate': values[4],
+                'insurance_name': values[6],
+                'basic_insurance_name': values[12],
+            }
+        elif data_list := response.css('main script[type="text/javascript"]').re(r'\[.*\]'):
+            yield from json5.loads(data_list[0])
+        else:
+            yield {'status': 'not valid'}
