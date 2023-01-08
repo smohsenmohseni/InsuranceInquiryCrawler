eb74b8401abbb3c2095e2ba8f5d3f2ec9c39966e
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-08 09:31:36 +0330

    FEAT: add flake8 as static code analytics

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -3,9 +3,9 @@
 THERE_IS_NO_CONFIGURATION_FILE='There is no configuration file.'
 
 
-if [ ! -f ./pyproject.toml ] || [ ! -f ./.isort.cfg ] || [ ! -f ./mypy.ini ]
+if [ ! -f ./pyproject.toml ] || [ ! -f ./.isort.cfg ] || [ ! -f ./mypy.ini ] || [ ! -f ./.flake8 ]
 then
   echo -e "\n\033[31m${THERE_IS_NO_CONFIGURATION_FILE}\033[0m\n"
 else
-  isort . && echo -e \\n; black . && echo -e \\n; mypy .;
+  isort . && echo -e \\n; black . && echo -e \\n; mypy . && echo -e \\n; flake8 .
 fi
