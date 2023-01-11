d5e04f5e9a89790cdb516287a07becdeb2536667
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2023-01-11 12:31:48 +0330

    FIX: add default value for getenv

--- app/constants/info.py
+++ app/constants/info.py
@@ -6,8 +6,8 @@ ATIEH_INSURANCE_INFO = {
     'login_url': 'https://rasatpa.ir/sso/login?service=https%3A%2F%2Frasatpa.ir%2Fhcp%2Flogin%2Fcas',
     'inquiry_url': 'https://rasatpa.ir/hcp/reception/inquiryInsuredPerson',
     'login_data': {
-        'username': str(os.getenv('ATIEH_USERNAME')),
-        'password': str(os.getenv('ATIEH_PASSWORD')),
+        'username': str(os.getenv('ATIEH_USERNAME', '')),
+        'password': str(os.getenv('ATIEH_PASSWORD', '')),
         'execution': 'e1s1',
         '_eventId': 'submit',
     },
@@ -21,8 +21,8 @@ DANA_INSURANCE_INFO = {
         'GetDataBimenameBimeShodeFanByCodeMeliTarikh?tarikhHazine=1401/10/13&CodeMelli={national_code}'
     ),
     'login_data': {
-        'NameKarbari': str(os.getenv('DANA_USERNAME')),
-        'RamzeObor': str(os.getenv('DANA_PASSWORD')),
+        'NameKarbari': str(os.getenv('DANA_USERNAME', '')),
+        'RamzeObor': str(os.getenv('DANA_PASSWORD', '')),
     },
 }
 
@@ -33,8 +33,8 @@ IRAN_INSURANCE_INFO = {
     ),
     'inquiry_url': 'http://darman.iraninsurance.ir/home-flow?execution=e1s1',
     'login_data': {
-        'username': str(os.getenv('IRAN_USERNAME')),
-        'password': str(os.getenv('IRAN_PASSWORD')),
+        'username': str(os.getenv('IRAN_USERNAME', '')),
+        'password': str(os.getenv('IRAN_PASSWORD', '')),
     },
 }
 
@@ -47,8 +47,8 @@ MAD_ASIA_INSURANCE_INFO = {
     'login_data': {
         'scope': 'openid profile user_info',
         'grant_type': 'password',
-        'username': str(os.getenv('MAD_USERNAME')),
-        'password': str(os.getenv('MAD_PASSWORD')),
+        'username': str(os.getenv('MAD_USERNAME', '')),
+        'password': str(os.getenv('MAD_PASSWORD', '')),
         'client_id': 'MCClaimProc-ResOwner',
         'client_secret': 'secret',
     },
